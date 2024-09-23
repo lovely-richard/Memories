523644203ff9c9e0e1cf36e0fee191777e233da6
lovely-richard <lovely_richard319@outlook.com>
2024-09-23 07:49:15 +0200

    Created profile reducer and store

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -3,3 +3,9 @@ export const UPDATE = 'UPDATE';
 export const DELETE = 'DELETE';
 export const FETCH_ALL = 'FETCH_ALL';
 export const LIKE = 'LIKE';
+export const GET_PROFILE = 'GET_PROFILE';
+export const CREATE_PROFILE = 'CREATE_PROFILE';
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
+export const DELETE_PROFILE = "DELETE_PROFILE";
+
+
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
 
 import posts from './posts';
+import profile from './profile';
 
-export const reducers = combineReducers({ posts });
+export const reducers = combineReducers(
+    { posts: posts, 
+      profile: profile
+     });
